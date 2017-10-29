72942592c15e23602e2e29101826b018cfa14148
Artur Kowalczyk <fizjokowalczyk@gmail.com>
2017-10-29 12:12:14 +0100

    add comments in js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// The window where you need write your name:
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+alert('Hello, ' + name); // This is alert window
+console.log('Hello, ' + name); // It's window where the vivible text is 'Hello+ user name'
